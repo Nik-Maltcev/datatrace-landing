f24e10a7dd0d66f07b11ed4e1187eb7c5a39a63e
Nik-Maltcev <35464161+Nik-Maltcev@users.noreply.github.com>
2025-10-05 20:47:27 +0300

    feat: обновили заголовок и описание сайта

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'DataTrace - сервис по удалению личной информации из утечек',
+  description: 'ИИ-платформа для поиска и удаления скомпрометированной личной информации из баз данных злоумышленников',
 }
 
 export default function RootLayout({
